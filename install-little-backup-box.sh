ac575962a7a6e5525dff7e71a4a4e8f2d7ac2954
Markus1995 <gutmannmarkus1@gmail.com>
2016-07-04 18:00:36 +0200

    Update install-little-backup-box.sh

--- install-little-backup-box.sh
+++ install-little-backup-box.sh
@@ -11,6 +11,7 @@ sudo chmod 755 *.sh
 crontab -l | { cat; echo "#@reboot sudo /home/pi/little-backup-box/cardbackup.sh >> /home/pi/little-backup-box/cardbackup.log"; } | crontab -
 crontab -l | { cat; echo "#@reboot sudo /home/pi/little-backup-box/camerabackup.sh >> /home/pi/little-backup-box/camerabackup.log"; } | crontab -
 crontab -l | { cat; echo "#@reboot sudo /home/pi/little-backup-box/cardcamerabackup.sh >> /home/pi/little-backup-box/cardcamerabackup.log"; } | crontab
+crontab -l | { cat; echo "#@reboot sudo /home/pi/little-backup-box/cardandcamera.sh >> /home/pi/little-backup-box/cardandcamera.log"; } | crontab
 
 sudo pip install blinkstick
 sudo blinkstick --add-udev-rule
