5712bb7c001b45f23f76289cdd2c33ab6169727e
Markus1995 <gutmannmarkus1@gmail.com>
2016-07-04 17:57:34 +0200

    Update install-little-backup-box.sh

--- install-little-backup-box.sh
+++ install-little-backup-box.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-apt-get update && apt-get dist-upgrade -y && apt-get install git-core rsync gphoto2 python-pip -y
+sudo apt-get update && sudo apt-get dist-upgrade -y && sudo apt-get install git-core rsync gphoto2 python-pip -y
 
-mkdir /media/card
-mkdir /media/storage
+sudo mkdir /media/card
+sudo mkdir /media/storage
 
 git clone https://github.com/dmpop/little-backup-box.git
 cd little-backup-box
-chmod 755 *.sh
-crontab -l | { cat; echo "#@reboot /home/pi/little-backup-box/cardbackup.sh >> /home/pi/little-backup-box/cardbackup.log"; } | crontab -
-crontab -l | { cat; echo "#@reboot /home/pi/little-backup-box/camerabackup.sh >> /home/pi/little-backup-box/camerabackup.log"; } | crontab -
-crontab -l | { cat; echo "#@reboot /home/pi/little-backup-box/cardcamerabackup.sh >> /home/pi/little-backup-box/cardcamerabackup.log"; } | crontab
+sudo chmod 755 *.sh
+crontab -l | { cat; echo "#@reboot sudo /home/pi/little-backup-box/cardbackup.sh >> /home/pi/little-backup-box/cardbackup.log"; } | crontab -
+crontab -l | { cat; echo "#@reboot sudo /home/pi/little-backup-box/camerabackup.sh >> /home/pi/little-backup-box/camerabackup.log"; } | crontab -
+crontab -l | { cat; echo "#@reboot sudo /home/pi/little-backup-box/cardcamerabackup.sh >> /home/pi/little-backup-box/cardcamerabackup.log"; } | crontab
 
-pip install blinkstick
-blinkstick --add-udev-rule
+sudo pip install blinkstick
+sudo blinkstick --add-udev-rule
 
 read -p "Connect your the camera and press [Enter]..."
 CAMERA =$(gphoto2 --auto-detect | sed -n '3p' | cut -d ' ' -f 1)
