1a7964f10eb209a80642df84927ff9af97da8932
Markus1995 <gutmannmarkus1@gmail.com>
2016-07-04 17:59:01 +0200

    Update cardandcamera.sh

--- cardandcamera.sh
+++ cardandcamera.sh
@@ -10,7 +10,7 @@
 # Save the crontab file.
 
 # Set the ACT LED to heartbeat
-sh -c "echo heartbeat > /sys/class/leds/led0/trigger"
+sudo sh -c "echo heartbeat > /sys/class/leds/led0/trigger"
 
 # User-defined settings
 CAMERA_SEARCH_STRING="USB" # Run the gphoto2 --auto-detect command. Use the first word of the camera maker's name.
@@ -34,8 +34,8 @@ done
 mount /dev/$STORAGE_DEV $STORAGE_PATH
 
 # Set the ACT LED to blink at 1000ms to indicate that the storage device has been mounted
-sh -c "echo timer > /sys/class/leds/led0/trigger"
-sh -c "echo 1000 > /sys/class/leds/led0/delay_on"
+sudo sh -c "echo timer > /sys/class/leds/led0/trigger"
+sudo sh -c "echo 1000 > /sys/class/leds/led0/delay_on"
 
 # Wait for a card reader or a camera
 CARD_READER=$(ls /dev/* | grep $CARD_DEV | cut -d"/" -f3)
@@ -52,7 +52,7 @@ if [ ! -z $CARD_READER ]; then
   mount /dev/$CARD_DEV $CARD_PATH
   UUID=$(blkid -s UUID -o value /dev/$CARD_DEV)
   # Set the ACT LED to blink at 500ms to indicate that the card has been mounted
-  sh -c "echo 500 > /sys/class/leds/led0/delay_on"
+  sudo sh -c "echo 500 > /sys/class/leds/led0/delay_on"
   # If UUID doesn't exist, read the id file on the card and use it as a directory name in the backup path
   # Otherwise use the UUID as a directory name in the backup path
   if [ -z $UUID ]; then
@@ -63,9 +63,8 @@ if [ ! -z $CARD_READER ]; then
   fi
 # Perform backup using rsync
 rsync -avh $CARD_PATH/ $BACKUP_PATH
-# Blink firebrick red to indicate that the backup is completed
-blinkstick --index 0 --repeats 3 --blink firebrick
-blinkstick --index 1 --set-color=firebrick
+# Turn off the ACT LED to indicate that the backup is completed
+sudo sh -c "echo 0 > /sys/class/leds/led0/brightness"
 fi
 
 #If the camera is detected, transfer photos using gPhoto2
@@ -76,7 +75,7 @@ if [ ! -z $CAMERA ]; then
 cd $CAMERA_BACKUP_PATH
 gphoto2 --get-all-files --skip-existing
 # Turn off the ACT LED to indicate that the backup is completed
-sh -c "echo 0 > /sys/class/leds/led0/brightness"
+sudo sh -c "echo 0 > /sys/class/leds/led0/brightness"
 fi
 # Shutdown Raspberry Pi
 shutdown -h now
