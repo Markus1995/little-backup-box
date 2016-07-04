8247be28891efb3a725eb885f151b2c2f1297dec
Markus1995 <gutmannmarkus1@gmail.com>
2016-07-04 17:52:41 +0200

    Update and rename cardcamerabackup.sh to cardandcamera.sh

--- cardandcamera.sh
+++ cardandcamera.sh
@@ -9,10 +9,8 @@
 # @reboot sudo /path/to/cardcamerabackup.sh
 # Save the crontab file.
 
-# If BlinkStick (https://www.blinkstick.com) is installed,
-# Blink green to indicate that the script is ready
-blinkstick --index 0 --repeats 3 --blink green
-blinkstick --index 1 --set-color=green
+# Set the ACT LED to heartbeat
+sh -c "echo heartbeat > /sys/class/leds/led0/trigger"
 
 # User-defined settings
 CAMERA_SEARCH_STRING="USB" # Run the gphoto2 --auto-detect command. Use the first word of the camera maker's name.
@@ -35,9 +33,9 @@ done
 # When the USB storage device is detected, mount it
 mount /dev/$STORAGE_DEV $STORAGE_PATH
 
-# Blink turquoise to indicate that the storage device has been mounted
-blinkstick --index 0 --repeats 3 --blink turquoise
-blinkstick --index 1 --set-color=turquoise
+# Set the ACT LED to blink at 1000ms to indicate that the storage device has been mounted
+sh -c "echo timer > /sys/class/leds/led0/trigger"
+sh -c "echo 1000 > /sys/class/leds/led0/delay_on"
 
 # Wait for a card reader or a camera
 CARD_READER=$(ls /dev/* | grep $CARD_DEV | cut -d"/" -f3)
@@ -53,9 +51,8 @@ done
 if [ ! -z $CARD_READER ]; then
   mount /dev/$CARD_DEV $CARD_PATH
   UUID=$(blkid -s UUID -o value /dev/$CARD_DEV)
-  # Blink yellow to indicate that the storage device has been mounted
-  blinkstick --index 0 --repeats 3 --blink yellow
-  blinkstick --index 1 --set-color=yellow
+  # Set the ACT LED to blink at 500ms to indicate that the card has been mounted
+  sh -c "echo 500 > /sys/class/leds/led0/delay_on"
   # If UUID doesn't exist, read the id file on the card and use it as a directory name in the backup path
   # Otherwise use the UUID as a directory name in the backup path
   if [ -z $UUID ]; then
@@ -78,9 +75,8 @@ if [ ! -z $CAMERA ]; then
   fi
 cd $CAMERA_BACKUP_PATH
 gphoto2 --get-all-files --skip-existing
-# Blink firebrick red to indicate that the backup is completed
-blinkstick --index 0 --repeats 3 --blink firebrick
-blinkstick --index 1 --set-color=firebrick
+# Turn off the ACT LED to indicate that the backup is completed
+sh -c "echo 0 > /sys/class/leds/led0/brightness"
 fi
 # Shutdown Raspberry Pi
 shutdown -h now
