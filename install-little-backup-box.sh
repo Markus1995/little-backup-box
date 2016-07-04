a46c55239ba8ced21e49499e7cdccbff07f2288f
Markus1995 <gutmannmarkus1@gmail.com>
2016-07-04 17:42:59 +0200

    Update install-little-backup-box.sh
    Sudo entfernt

--- install-little-backup-box.sh
+++ install-little-backup-box.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-sudo apt-get update && sudo apt-get dist-upgrade -y && sudo apt-get install git-core rsync gphoto2 python-pip -y
+apt-get update && apt-get dist-upgrade -y && apt-get install git-core rsync gphoto2 python-pip -y
 
-sudo mkdir /media/card
-sudo mkdir /media/storage
+mkdir /media/card
+mkdir /media/storage
 
 git clone https://github.com/dmpop/little-backup-box.git
 cd little-backup-box
-sudo chmod 755 *.sh
+chmod 755 *.sh
 crontab -l | { cat; echo "#@reboot sudo /home/pi/little-backup-box/cardbackup.sh >> /home/pi/little-backup-box/cardbackup.log"; } | crontab -
 crontab -l | { cat; echo "#@reboot sudo /home/pi/little-backup-box/camerabackup.sh >> /home/pi/little-backup-box/camerabackup.log"; } | crontab -
 crontab -l | { cat; echo "#@reboot sudo /home/pi/little-backup-box/cardcamerabackup.sh >> /home/pi/little-backup-box/cardcamerabackup.log"; } | crontab
 
-sudo pip install blinkstick
-sudo blinkstick --add-udev-rule
+pip install blinkstick
+blinkstick --add-udev-rule
 
 read -p "Connect your the camera and press [Enter]..."
 CAMERA =$(gphoto2 --auto-detect | sed -n '3p' | cut -d ' ' -f 1)
